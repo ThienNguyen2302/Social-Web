d149a5348da40a9b0ec616b68d8534fe44d67fb7
ThienNguyen2302 <nguyenngocthien749@gmail.com>
2023-11-11 20:49:48 +0700

    feu: register and login for user

---
+++ src/core/middlewares/validation.middleware.ts
@@ -0,0 +1,27 @@
+import { NextFunction, Request, RequestHandler, Response } from 'express';
+import { ValidationError, validate } from 'class-validator';
+
+import { HttpException } from '../exceptions';
+import { plainToInstance } from 'class-transformer';
+
+const validationMiddleware = (
+  type: any,
+  skipMissingProperties = false
+): RequestHandler => {
+  return (req: Request, res: Response, next: NextFunction) => {
+    validate(plainToInstance(type, req.body), { skipMissingProperties }).then(
+      (errors: ValidationError[]) => {
+        if (errors.length > 0) {
+          const messages = errors
+            .map((error: ValidationError) => {
+              return Object.values(error.constraints!);
+            })
+            .join(', ');
+          next(new HttpException(400, messages));
+        }
+      }
+    );
+  };
+};
+
+export default validationMiddleware;
